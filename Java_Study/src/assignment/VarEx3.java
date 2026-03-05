39a93ca3b27b8da3bea0bfec201a2a333d7864be
gituser <danjee2009@gmail.com>
2026-03-05 19:36:25 +0900

    commit

---
+++ Java_Study/src/assignment/VarEx3.java
@@ -0,0 +1,10 @@
+package assignment;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long a = 1000000000;
+        boolean b = true;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ Java_Study/src/variable/TypeCasting3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class TypeCasting3 {
+    public static void main(String[] args) {
+        float value1 = 3;
+        long value2 = 5;
+        double value3 = 7;
+        byte value4 = 9;
+        short value5 = 11;
+    }
+}
