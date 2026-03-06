cd34b5ab4b48cceedab4e3d9400d6c785c496664
gituser <danjee2009@gmail.com>
2026-03-06 12:38:34 +0900

    3/6 4교시 수업 내용

---
+++ Java_Study/src/operator/OperateorAdd1.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class OperateorAdd1 {
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(--a);
+        System.out.println(a--);
+    }
+}
